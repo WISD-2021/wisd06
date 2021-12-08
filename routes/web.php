b163224abc6151d2be801288196c928a8d4a95a0
Chen Pei An <3a832009@gm.student.ncut.edu.tw>
2021-12-08 12:04:22 +0800

    cc.c1 & cc.c2

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,10 @@ Route::get('/aa/a2', function () {
 Route::get('/bb/b1',function(){
     return view('bb.b1');
 });
+
+Route::get('/cc/c1', function () {
+    return view('cc.c1');
+});
+Route::get('/cc/c2', function () {
+    return view('cc.c2');
+});
